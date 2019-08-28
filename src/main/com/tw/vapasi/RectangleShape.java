d93a2c5dc4cf9941146b84379e60e3692375063b
“mayura” <mayurakharche@gmail.com>
2019-08-28 12:37:01 +0530

    [mayura,srividya] Add. Area Test

--- src/main/com/tw/vapasi/RectangleShape.java
+++ src/main/com/tw/vapasi/RectangleShape.java
@@ -1,9 +1,23 @@
 package com.tw.vapasi;
 
+//Deals with four side shape with four perpendicular angle.
 public class RectangleShape {
-    public boolean isLengthGreaterThanWidth(int length, int width) {
-        if(length > width)
+    private int length;
+    private int width;
+
+    public RectangleShape(int length, int width)
+    {
+        this.length = length;
+        this.width = width;
+    }
+
+    public boolean isLengthGreaterThanWidth() {
+        if(this.length > this.width)
             return true;
         return false;
+    }
+
+    public int area() {
+        return this.length * this.width;
     }
 }
--- src/test/RectangleShapeTest.java
+++ src/test/RectangleShapeTest.java
@@ -1,17 +1,21 @@
 import com.tw.vapasi.RectangleShape;
 import org.junit.Test;
 
-import java.awt.*;
-
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class RectangleShapeTest {
     @Test
     public void IsLengthGreaterThanWidthTest()
     {
-        RectangleShape rectangle = new RectangleShape();
-        assertEquals(true, rectangle.isLengthGreaterThanWidth(6,3));
-
+        RectangleShape rectangle = new RectangleShape(6,3);
+        assertTrue(rectangle.isLengthGreaterThanWidth());
     }
 
+    @Test
+    public void isArea8WithInput2and4()
+    {
+        RectangleShape rectangle = new RectangleShape(4,2);
+        assertEquals(8, rectangle.area());
+    }
 }
